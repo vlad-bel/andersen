7f6f1a2f4c03c3d05dae15bd67bde83171190754
vladislavbelenkov@attractgroup.com <630245abc>
2022-04-05 13:10:22 +0400

    dev_03_signin
    - established datalist ui & logic
    - add pagination
    - add refresh

---
+++ lib/data/models/comment.dart
@@ -0,0 +1,56 @@
+// To parse this JSON data, do
+//
+//     final comment = commentFromJson(jsonString);
+
+import 'dart:convert';
+
+class Comment {
+  Comment({
+    this.postId,
+    this.id,
+    this.name,
+    this.email,
+    this.body,
+  });
+
+  final int? postId;
+  final int? id;
+  final String? name;
+  final String? email;
+  final String? body;
+
+  Comment copyWith({
+    int? postId,
+    int? id,
+    String? name,
+    String? email,
+    String? body,
+  }) =>
+      Comment(
+        postId: postId ?? this.postId,
+        id: id ?? this.id,
+        name: name ?? this.name,
+        email: email ?? this.email,
+        body: body ?? this.body,
+      );
+
+  factory Comment.fromRawJson(String str) => Comment.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Comment.fromJson(Map<String, dynamic> json) => Comment(
+        postId: json["postId"],
+        id: json["id"],
+        name: json["name"],
+        email: json["email"],
+        body: json["body"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "postId": postId,
+        "id": id,
+        "name": name,
+        "email": email,
+        "body": body,
+      };
+}
--- lib/flows/app/datalist/datalist_cubit.dart
+++ lib/flows/app/datalist/datalist_cubit.dart
@@ -1,7 +1,89 @@
+import 'package:andersen_test1/data/comments/interactor/comments_interactor.dart';
+import 'package:andersen_test1/data/models/app_error.dart';
+import 'package:andersen_test1/data/models/comment.dart';
+import 'package:andersen_test1/service/app_notifier.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+import 'package:get_it/get_it.dart';
 
-class DataListState {}
+abstract class DataListState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
+class DataListLoading extends DataListState {}
+
+class DataListSuccess extends DataListState {
+  final bool pagination;
+  final List<Comment> commentsList;
+
+  DataListSuccess({
+    required this.commentsList,
+    this.pagination = false,
+  });
+
+  @override
+  List<Object?> get props => [
+        commentsList,
+        pagination,
+      ];
+}
+
+class DataListError extends DataListState {}
+
+@singleton
 class DataListCubit extends Cubit<DataListState> {
-  DataListCubit() : super(DataListState());
+  DataListCubit() : super(DataListLoading()) {
+    init();
+  }
+
+  final commentsInteractor = GetIt.instance.get<CommentsInteractor>();
+
+  var page = 1;
+  final limit = 20;
+
+  Future init() async {
+    emit(DataListLoading());
+    try {
+      final commentsList = await commentsInteractor.getComments(
+        page: page,
+        limit: limit,
+      );
+
+      emit(DataListSuccess(commentsList: commentsList));
+    } on AppError catch (e) {
+      emit(DataListError());
+      showMessage(message: e.error, type: NotifyType.error);
+    }
+  }
+
+  Future<void> paginateComments() async {
+    try {
+      var currState = state as DataListSuccess;
+      final comments = currState.commentsList;
+
+      emit(DataListSuccess(
+        pagination: true,
+        commentsList: comments,
+      ));
+
+      page++;
+
+      final newComments = await commentsInteractor.getComments(
+        page: page,
+        limit: limit,
+      );
+      await Future.delayed(const Duration(seconds: 1));
+
+      comments.addAll(newComments);
+
+      emit(DataListSuccess(
+        commentsList: comments,
+      ));
+    } on AppError catch (e) {
+      emit(DataListError());
+      showMessage(message: e.error, type: NotifyType.error);
+    }
+  }
 }
